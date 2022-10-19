adefcd89d25da2730c909ef428fee0fd05cb762e
Keke <dieketsengmafatle98@gmail.com>
2022-10-18 18:36:53 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-   printf("with proper grammar,but the otcome is a piece of art,\n");
-   return (0);
+       	printf("with proper grammar,but the otcome is a piece of art,\n");
+	return (0);
 }   
